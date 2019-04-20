825a746227005513fb137185c0881c5e68e1d4d8
Seth Austin Harding <gearladeldsvon@gmail.com>
2019-04-20 10:33:47 +0800

    Update DmesgTest.c

--- DmesgTest.c
+++ DmesgTest.c
@@ -17,8 +17,17 @@ ST - the start time of this process in the format seconds.nanoseconds.
 FT - the finish time of this process in the format seconds.nanoseconds.
 
 ex: [Project1] 4007 1394530429.657899070 1394530430.004979285
+
+ProcessInfo contents
+    int ready_time;
+    int time_needed; // Same as execution time in the problem description i.e. time needed to run the process.
+    int remaining_time; // Remaining time for the process; Use in PSJF to determine the process to be run.
+    pid_t pid;
+    ProcessStatus status;
+    char *name;
 */
 
+      
   printf(KERN_INFO "\n";
 }
 
