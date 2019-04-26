fa5542cb484f23c08d31df84aa50bb4db554de58
Seth Austin Harding <gearladeldsvon@gmail.com>
2019-04-26 16:05:30 +0800

    Update SJF.c

--- SJF.c
+++ SJF.c
@@ -1,3 +1,19 @@
 #include "scheduler.h"
 
+void add_process_SJF(ProcessInfo *new_process) {
+     
+}
+
+void remove_current_process_SJF(void) {
+     
+}
+
+void context_switch_SJF(void) {
+     
+}
+
+bool scheduler_empty_SJF(void) {
+     
+}
+
 // Please read comments in scheduler.h to see what functions to be implemented.
