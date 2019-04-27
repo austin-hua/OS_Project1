ccf0f11dc69f3fcfa3fdb6072142f85a6a0ff019
Seth Austin Harding <gearladeldsvon@gmail.com>
2019-04-27 19:09:54 +0800

    Update RR.c

--- RR.c
+++ RR.c
@@ -1,7 +1,15 @@
+#include <stdlib.h>
 #include "scheduler.h"
 
+static ProcessInfo **pq;
+int num_process;
+
+static ProcessInfo *activeProcess;
+
 void set_strategy_RR(void) {
-    
+    pq = (ProcessInfo **) malloc(sizeof(ProcessInfo *) * num_process;
+    num_process = 0;
+    activeProcess = NULL;
 }
 
 void add_process_RR(void) {
@@ -9,7 +17,7 @@ void add_process_RR(void) {
 }
 
 void remove_current_process_RR(void) {
-    
+    activeProcess = NULL;
 }
 
 void timeslice_over_RR(void) {
@@ -17,9 +25,11 @@ void timeslice_over_RR(void) {
 }
 
 void context_switch_RR(void) {
-    
+    if(activeProcess != NULL) {
+        
+    }
 }
 
 void scheduler_empty_RR(void) {
-    
+    return activeProcess == NULL && num_process == 0;
 }
