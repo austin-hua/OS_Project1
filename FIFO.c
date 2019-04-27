4e266f924cd7d4d99b886560ef350a7a5244c30e
b05902132 <b05902132@csie.ntu.edu.tw>
2019-04-28 03:00:44 +0800

    bugfix

--- FIFO.c
+++ FIFO.c
@@ -2,27 +2,25 @@
 #include <signal.h>
 #include <stdlib.h>
 
-static ProcessInfo **pq;
 static int active_process_id = -1;
 static int prev_process_id = -1;
 static int count_process = 0;
 
 void set_strategy_FIFO() {
-	
 }
 
-void add_process_FIFO(ProcessInfo *) {
-	pq = (ProcessInfo **) malloc(sizeof(ProcessInfo *) * num_process);
+void add_process_FIFO(ProcessInfo *p) {
+    count_process++;
 }
 
-void remove_current_process_RR(void) {
-	
+void remove_current_process_FIFO(void) {
+    count_process--;
+
 }
 
 void context_switch_FIFO(void) {
-	
 }
 
 bool scheduler_empty_FIFO(void) {
-	
+    return count_process == 0;
 }
