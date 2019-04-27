cd2818c6ee877c1c249b871e37146c4cb37980de
b05902132 <b05902132@csie.ntu.edu.tw>
2019-04-27 18:50:08 +0800

    fix compilation process

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 CC=gcc
 CFLAGS=-Wall -Wextra -O2
 LDFLAGS=-lrt
-main: main.o FIFO.o RR.o SJF.o PSJF.o
+main: main.o FIFO.o RR.o SJF.o PSJF.o heap.o
--- heap.c
+++ heap.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "scheduler.h"
 
 void heap_init(Heap* h) {
@@ -6,7 +7,7 @@ void heap_init(Heap* h) {
 }
 
 static void heap_swap(Heap *h, int lhs, int rhs) {
-     ProcessInfo *temp = p->pq[lhs];
+     ProcessInfo *temp = h->pq[lhs];
      h->pq[lhs] = h->pq[rhs];
      h->pq[rhs] = temp;
 }
@@ -14,16 +15,17 @@ static void heap_swap(Heap *h, int lhs, int rhs) {
 static void upheap(Heap *h, int childInd) {
      int parentInd = (childInd - 1) / 2;
      while (parentInd != 0 && h->pq[parentInd]->remaining_time < h->pq[childInd]->remaining_time) {
-          heap_swap(parentInd, childInd);    
+          heap_swap(h, parentInd, childInd);
           childInd = parentInd;
-          parentInd = (childInd - 1) / 2;     
+          parentInd = (childInd - 1) / 2;
      }
 }
 
 void heap_insert(Heap *h, ProcessInfo *p) {
      int childInd = h->heap_size;
+     h->pq[childInd] = p;
      h->heap_size++;
-     upheap(h, heap_size-1);
+     upheap(h,childInd);
 }
 
 ProcessInfo *heap_top(Heap *h) {
@@ -38,25 +40,25 @@ static int min_child(Heap *h, int parentInd) {
           return child1Ind;
      }
      if (h->pq[child1Ind]->remaining_time > h->pq[child2Ind]->remaining_time){
-         return child2ind;
+         return child2Ind;
      }
      return child1Ind;
 }
 
 static void downheap(Heap *h, int parentInd) {
      while(parentInd * 2 < h->heap_size) {
-         int minChildInd = min_child(parentInd);
+         int minChildInd = min_child(h,parentInd);
          if (h->pq[parentInd]->remaining_time <= h->pq[minChildInd]->remaining_time){
              break;
          }
-         swap(h, parentInd, minChildInd);
+         heap_swap(h, parentInd, minChildInd);
          parentInd = minChildInd;
      }
 }
 
 void heap_pop(Heap *h) {
-     swap(h, heap_size, 0);
      h->heap_size--;
+     heap_swap(h, h->heap_size, 0);
      downheap(h, 0);
 }
 
