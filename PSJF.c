454b17787290edd36460a85c90d306b54bd0b854
b05902132 <b05902132@csie.ntu.edu.tw>
2019-04-30 22:14:46 +0800

    bugfix

--- PSJF.c
+++ PSJF.c
@@ -10,8 +10,16 @@ static ProcessInfo *active_process = NULL;
 static int last_context_switch_time = 0;
 static int current_time = 0;
 
+/* Invariant: the top of heap should be always the currently running process
+ * after context_switch_PSJF() is called.
+ */
+
 void add_process_PSJF(ProcessInfo *new_process) {
+    assert(active_process == heap_top(&pq));
     current_time = new_process->arrival_time;
+    active_process->remaining_time -= (current_time - last_context_switch_time);
+    // active_process doesn't need to be popped as deducting its remaining time 
+    // does not require updating the heap.
     heap_insert(&pq, new_process);
 }
 
@@ -28,10 +36,6 @@ void remove_current_process_PSJF(void) {
 }
 
 void context_switch_PSJF(void) {
-    if(active_process != NULL){
-        active_process->remaining_time -= (current_time - last_context_switch_time);
-    }
-
     if (!heap_empty(&pq) && active_process != heap_top(&pq)){
         if (active_process != NULL){
             suspend_process(active_process->pid);
